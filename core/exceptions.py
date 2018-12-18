f9017dd8669c64e91ad32f7d833946c3c03bfd2b
Blaine F Garrett <blaine@blainegarrett.com>
2018-12-17 18:12:51 -0600

    Adding Conflict ExceptionType

--- core/exceptions.py
+++ core/exceptions.py
@@ -4,19 +4,27 @@
 class PermissionException(Exception):
     """
     Internal Exception when permission to an object cannot be granted
-    Any handler catching this error should return a 403 response code
+    Any HTTP Handler catching this error should set a 403 response code
     """
 
 
 class AuthenticationException(Exception):
     """
     Internal Exception when an authentication attempt fails
-    Any handler catching this error should return a 401 response code
+    Any HTTP Handler catching this error should set a 401 response code
     """
 
 
 class DoesNotExistException(Exception):
     """
     Internal exception when an object cannot be found
-    Any handler catching this error should return a 404 response code
+    Any HTTP Handler catching this error should set a 404 response code
+    """
+
+
+class ConflictException(Exception):
+    """
+    Internal Exception when an obect could not be created or mutated due to a conflict, such
+    as when the object already existsself.
+    Any HTTP handler catching this error should set a 409 response code
     """
